9144db1d4c9c8548c6f1b986943b1bfdd63f2bbb
Yotsapat Punyaworapan <110213795+pookantong@users.noreply.github.com>
2023-01-09 23:33:37 +0700

    Update lab1_5.py

--- lab1_5.py
+++ lab1_5.py
@@ -1,6 +1,7 @@
+lists = []
 for x in range(999, 1, -1):
     for y in range(999, 1, -1):
         txt = str(x*y)
         if txt == txt[::-1]:
-            print(x, y, txt)
-            exit()
+            lists.append(x*y)
+print(max(lists))
