8a885eb82933689663397231098ef785be40ca5f
Yotsapat Punyaworapan <110213795+pookantong@users.noreply.github.com>
2023-01-11 16:26:49 +0700

    -

--- lab1/lab1_2.py
+++ lab1/lab1_2.py
@@ -2,9 +2,9 @@ small = 0
 big = 0
 strings = input()
 for x in range(len(strings)):
-    if strings[x].isupper() == True:
+    if strings[x].isupper():
         big = big + 1
-    if strings[x].islower() == True:
+    if strings[x].islower():
         small = small + 1 
 print(small)
 print(big)
--- lab1/lab1_5.py
+++ lab1/lab1_5.py
@@ -1,7 +1,8 @@
 lists = []
-for x in range(1, 1000):
-    for y in range(1, 1000):
+for x in range(100, 1000):
+    for y in range(100, 1000):
         txt = str(x*y)
         if txt == txt[::-1]:
+            print(x,y,txt)
             lists.append(x*y)
 print(max(lists))
