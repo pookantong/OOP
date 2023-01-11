4f70c98d4c5a77b981b3972b5443d86ae3d078e3
Yotsapat Punyaworapan <110213795+pookantong@users.noreply.github.com>
2023-01-11 14:16:02 +0700

    Update lab1_5.py

--- lab1/lab1_5.py
+++ lab1/lab1_5.py
@@ -1,6 +1,6 @@
 lists = []
-for x in range(1, 999):
-    for y in range(1, 999):
+for x in range(1, 1000):
+    for y in range(1, 1000):
         txt = str(x*y)
         if txt == txt[::-1]:
             lists.append(x*y)
