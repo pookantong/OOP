fc3b972b08ebd10ac4af0b0080d9f25072289917
Yotsapat Punyaworapan <110213795+pookantong@users.noreply.github.com>
2023-01-17 01:31:05 +0700

    -

--- test.py
+++
@@ -1,8 +0,0 @@
-def isEnglish(s):
-    try:
-        s.encode(encoding='utf-8').decode('ascii') 
-        except UnicodeDecodeError:
-        return False
-    else:
-        return True
-print(isEnglish(input()))
