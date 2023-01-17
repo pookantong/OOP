bd105b0b7d11349db6f2a1f1afe92ba294b13f75
Yotsapat Punyaworapan <110213795+pookantong@users.noreply.github.com>
2023-01-18 00:15:33 +0700

    -

--- lab2/lab2_9.py
+++ lab2/lab2_9.py
@@ -1,10 +1,5 @@
 def is_leap(year):
-    if (year % 400 == 0):
-        return True
-    elif (year % 4 ==0) and (year % 100 != 0):
-        return True
-    else:
-        return False
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
 def day_of_year(day, month, year):
     months = [31,28,31,30,31,30,31,31,30,31,30,31]
     days = day
