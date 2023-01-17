a853639704ad4272e9bf4e71b265ecc29fd082c6
Yotsapat Punyaworapan <110213795+pookantong@users.noreply.github.com>
2023-01-18 00:10:06 +0700

    Update lab2_1.py

--- lab2/lab2_1.py
+++ lab2/lab2_1.py
@@ -1,11 +1,5 @@
-list1 = []
 list1 = [int(item) for item in input().split()]
-for x in range(len(list1)):
-    for y in range(len(list1)):
-        if list1[x] < list1[y]:
-            temp = list1[y]
-            list1[y] = list1[x]
-            list1[x] = temp
+list1.sort()
 for i in range(len(list1)):
     if list1[0] == 0:
         temp = list1[0]
