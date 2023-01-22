0781877fd937c292352ae8b2386c18b7a19e7f7d
Yotsapat Punyaworapan <110213795+pookantong@users.noreply.github.com>
2023-01-22 16:58:57 +0700

    -

--- lab3/lab3_2.py
+++ lab3/lab3_2.py
@@ -5,3 +5,6 @@ def calc_average_score(data):
     return {[x for x in data.keys()][i] : ["{:.2f}".format(elem) for elem in [sum([score/len(data[x]) for score in data[x].values()]) for x in data.keys()]][i] for i in range(len(data))}
 print(add_score({ '65010895' : { 'python' : 50 }, '65010896' : {'python': 80, 'calculus': 90} },'65010895','calculus',60))
 print(calc_average_score(add_score({ '65010895' : { 'python' : 50 }, '65010896' : {'python': 80, 'calculus': 90} },'65010895','calculus',60)))
+'''def calc_average_score(data):
+    score = ["{:.2f}".format(elem) for elem in [sum([score/len(data[x]) for score in data[x].values()]) for x in data.keys()]]
+    return {[x for x in data.keys()][i] : score[i] for i in range(len(data))}'''
--- lab3/lab3_3.py
+++ lab3/lab3_3.py
@@ -1,2 +1,5 @@
 def is_plusone_dictionary(d):
     pass
+def calc_average_score(data):
+    score = ["{:.2f}".format(elem) for elem in [sum([score/len(data[x]) for score in data[x].values()]) for x in data.keys()]]
+    return {[x for x in data.keys()][i] : ["{:.2f}".format(elem) for elem in [sum([score/len(data[x]) for score in data[x].values()]) for x in data.keys()]][i] for i in range(len(data))}
