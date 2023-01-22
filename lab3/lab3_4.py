1f974911afbde355a3d2826a507371a90285e592
Yotsapat Punyaworapan <110213795+pookantong@users.noreply.github.com>
2023-01-23 00:09:11 +0700

    -

--- lab3/lab3_4.py
+++ lab3/lab3_4.py
@@ -1,3 +1,3 @@
 def char_count(str):
-    return {[x for x in str][i] : str.count([x for x in str][i]) for i in range(len(str))}
+    return {str[i] : str.count(str[i]) for i in range(len(str))}
 print(char_count('language'))
