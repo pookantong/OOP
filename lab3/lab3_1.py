a120e88dff9158915d64dd16cc70a173d22ae7d7
Yotsapat Punyaworapan <110213795+pookantong@users.noreply.github.com>
2023-01-22 23:19:17 +0700

    -

--- lab3/lab3_1.py
+++ lab3/lab3_1.py
@@ -3,4 +3,5 @@ def add_score(subject_score, subject, score):
     return subject_score
 def calc_average_score(data):
     return "{:.2f}".format(sum([x/len(data) for x in data.values()]))
+print(add_score({ 'python' : 50 },'calculus',60))
 print(calc_average_score(add_score({ 'python' : 50 },'calculus',60)))
--- lab3/lab3_5.py
+++ lab3/lab3_5.py
@@ -1,6 +1,9 @@
 def update_records(dictionary_record, id, property, value):
-    
-    return dictionary_record
+       if property == 'tracks':
+              dictionary_record[id][property] = value
+       else: 
+              dictionary_record
+
 record_collection = {2548: {'albumTitle': 'Slippery When Wet',
                             'artist': 'Bon Jovi',
                             'tracks': ['Let It Rock', 'You Give Love a Bad Name']},
@@ -9,4 +12,4 @@ record_collection = {2548: {'albumTitle': 'Slippery When Wet',
                             'tracks': ['1999', 'Little Red Corvette']},
                      1245: {'artist': 'Robert Palmer','tracks': []},
                      5439: {'albumTitle': 'ABBA Gold'}}
-print(update_records(record_collection, 5647, ))
+print(update_records(record_collection, 5439, 'artist', 'Pookantong'))
