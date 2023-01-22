767729c9bcc59222ca0d6ec5387bf6163c9238a8
Yotsapat Punyaworapan <110213795+pookantong@users.noreply.github.com>
2023-01-22 18:09:24 +0700

    -

---
+++ lab3/lab3_5.py
@@ -0,0 +1,12 @@
+def update_records(dictionary_record, id, property, value):
+    
+    return dictionary_record
+record_collection = {2548: {'albumTitle': 'Slippery When Wet',
+                            'artist': 'Bon Jovi',
+                            'tracks': ['Let It Rock', 'You Give Love a Bad Name']},
+                     2468: {'albumTitle': '1999',
+                            'artist': 'Prince',
+                            'tracks': ['1999', 'Little Red Corvette']},
+                     1245: {'artist': 'Robert Palmer','tracks': []},
+                     5439: {'albumTitle': 'ABBA Gold'}}
+print(update_records(record_collection, 5647, ))
