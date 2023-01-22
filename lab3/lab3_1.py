8a95fd27c3d58ab6cbcb9608b225ad4ef29ff38a
Yotsapat Punyaworapan <110213795+pookantong@users.noreply.github.com>
2023-01-22 23:48:37 +0700

    -

--- lab3/lab3_1.py
+++ lab3/lab3_1.py
@@ -2,6 +2,6 @@ def add_score(subject_score, subject, score):
     subject_score[subject] = score
     return subject_score
 def calc_average_score(data):
-    return "{:.2f}".format(sum([x/len(data) for x in data.values()]))
+    return "{:.2f}".format(sum(data.values())/len(data))
 print(add_score({ 'python' : 50 },'calculus',60))
 print(calc_average_score(add_score({ 'python' : 50 },'calculus',60)))
--- lab3/lab3_2.py
+++ lab3/lab3_2.py
@@ -2,6 +2,6 @@ def add_score(subject_score, student, subject, score):
     subject_score[student][subject] = score
     return subject_score
 def calc_average_score(data):
-    return {[x for x in data.keys()][i] : ["{:.2f}".format(elem) for elem in [sum([score/len(data[x]) for score in data[x].values()]) for x in data.keys()]][i] for i in range(len(data))}
+    return {[x for x in data.keys()][i] : ["{:.2f}".format(elem) for elem in [sum(data[x].values())/len(data[x]) for x in data.keys()]][i] for i in range(len(data))}
 print(add_score({ '65010895' : { 'python' : 50 }, '65010896' : {'python': 80, 'calculus': 90} },'65010895','calculus',60))
 print(calc_average_score(add_score({ '65010895' : { 'python' : 50 }, '65010896' : {'python': 80, 'calculus': 90} },'65010895','calculus',60)))
