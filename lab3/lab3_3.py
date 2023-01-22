042fad40f117ca781b681dc125c375ab224c36e1
Yotsapat Punyaworapan <110213795+pookantong@users.noreply.github.com>
2023-01-23 00:26:25 +0700

    -

--- lab3/lab3_3.py
+++ lab3/lab3_3.py
@@ -1,6 +1,6 @@
 def is_plusone_dictionary(d):
     for i in range(len(d)):
-        if [x for x in d.keys()][i] != [x for x in d.values()][i] - 1 or [x for x in d.keys()][i] != i*2+1:
+        if [x for x in d.keys()][i] != [x for x in d.values()][i] - 1 or [x for x in d.keys()][i] != i*2+[x for x in d.keys()][0]:
             return False
     return True
 print(is_plusone_dictionary({1:2, 3:4, 5:6, 7:8}))
