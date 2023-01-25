313c7a45ec8b2be827158402b197e3de0d30b723
Yotsapat Punyaworapan <110213795+pookantong@users.noreply.github.com>
2023-01-25 12:46:41 +0700

    -

--- lab3/lab3_1.py
+++ lab3/lab3_1.py
@@ -1,7 +1,7 @@
 def add_score(subject_score, subject, score):
-    subject_score[subject].update(score)
+    subject_score[subject] = score
     return subject_score
 def calc_average_score(data):
-    return "{:.2f}".format(sum(data.values())/len(data))
+    return f'{sum(data.values())/len(data):.2f}'
 print(add_score({ 'python' : 50, 'calculus' : 60 },'oop',42))
 print(calc_average_score(add_score({ 'python' : 50, 'calculus' : 60 },'oop',42)))
